842fbaddeecede4b4b652da346fa4df7f2889fe0
amadd123 <86683633+amadd123@users.noreply.github.com>
2022-07-28 10:35:34 -0400

    Add files via upload

---
+++ Graphs/Graphs/src/graphs/Graph.java
@@ -0,0 +1,20 @@
+package graphs;
+
+import java.util.*;
+
+/**
+ * Implements a graph. We use two maps: one map for adjacency properties 
+ * (adjancencyMap) and one map (dataMap) to keep track of the data associated 
+ * with a vertex. 
+ * 
+ * @author cmsc132
+ * 
+ * @param <E>
+ */
+public class Graph<E> {
+	/* You must use the following maps in your implementation */
+	private HashMap<String, HashMap<String, Integer>> adjacencyMap;
+	private HashMap<String, E> dataMap;
+
+
+}
